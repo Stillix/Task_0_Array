24c6bccaaf5c7086603aedfd953de6f6787d14f2
Илья Дорогокупец <stud1090352013@bntu.by>
2023-05-11 15:49:09 +0300

    Create Data parser

--- src/main/java/by/dorogokupets/arraytask/parser/DataParser.java
+++ src/main/java/by/dorogokupets/arraytask/parser/DataParser.java
@@ -1,20 +1,12 @@
 package by.dorogokupets.arraytask.parser;
 
-import by.dorogokupets.arraytask.validator.DataValidator;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DataParser {
-    private DataValidator validator = new DataValidator();
+        public List<String> parseData(String data) {
+            List<String> numbers = Arrays.asList(data.split("\\s+"));
+            return numbers;
 
-    public List<Integer> parseData(String data) {
-        List<Integer> integers = new ArrayList<>();
-        Arrays.stream(data.split("\\s+"))
-                .filter(s -> validator.isValid(s))
-                .forEach(i -> integers.add(Integer.parseInt(i)));
-        return integers;
     }
 }
-
