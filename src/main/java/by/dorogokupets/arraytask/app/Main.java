66953806bab94cecb142a4e5ccdad863994b4ca2
Илья Дорогокупец <stud1090352013@bntu.by>
2023-05-11 15:49:51 +0300

    Main

--- src/main/java/by/dorogokupets/arraytask/app/Main.java
+++ src/main/java/by/dorogokupets/arraytask/app/Main.java
@@ -1,26 +1,41 @@
 package by.dorogokupets.arraytask.app;
 
 
+import by.dorogokupets.arraytask.creater.ArrayCreator;
 import by.dorogokupets.arraytask.exception.DataException;
 import by.dorogokupets.arraytask.parser.DataParser;
 import by.dorogokupets.arraytask.reader.DataReader;
+import by.dorogokupets.arraytask.validator.DataValidator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         DataReader reader = new DataReader();
-        DataParser parser = new DataParser();
-        List<Integer> data;
+        String filename = "data\\dataarray.txt";
+
         try {
-            data = parser.parseData(reader.read("data\\dataarray.txt"));
-            System.out.println(data);
-        } catch (DataException e) {
-            try {
-                throw new DataException("Ошибка парсинга");
-            } catch (DataException ex) {
-                ex.printStackTrace();
+            String data = reader.read(filename);
+            DataParser parser = new DataParser();
+            List<String> numbers = parser.parseData(data);
+            DataValidator validator = new DataValidator();
+            List<Integer> validNumbers = new ArrayList<>();
+
+            for (String number : numbers) {
+                if (validator.stringNumberValidate(number)) {
+                    validNumbers.add(Integer.parseInt(number));
+                }
             }
+
+            ArrayCreator arrayCreator = new ArrayCreator();
+            int[] array = arrayCreator.createArray(validNumbers);
+
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+        } catch (DataException e) {
+            e.printStackTrace();
         }
     }
 }
