1b8c0c5eff0583965750b91a7cf410bf81439771
Илья Дорогокупец <stud1090352013@bntu.by>
2023-05-11 15:47:39 +0300

    Create array creator

--- src/main/java/by/dorogokupets/arraytask/creater/ArrayCreator.java
+++ src/main/java/by/dorogokupets/arraytask/creater/ArrayCreator.java
@@ -1,5 +1,21 @@
 package by.dorogokupets.arraytask.creater;
 
-public class ArrayCreator {
+import by.dorogokupets.arraytask.exception.DataException;
 
+import java.util.List;
+
+public class ArrayCreator {
+    public int[] createArray(List<Integer> numbers) throws DataException {
+        if (numbers.isEmpty()) {
+            throw new DataException("Empty list.");
+        }
+//        if (numbers.isEmpty()) {
+//            return new int[0];
+//        }
+        int[] array = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            array[i] = numbers.get(i);
+        }
+        return array;
+    }
 }
