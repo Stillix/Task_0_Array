67b0f0c0a463ab833c3c56e09f51434c0f1bfd56
Илья Дорогокупец <stud1090352013@bntu.by>
2023-05-18 16:55:35 +0300

    18.05

---
+++ src/main/java/by/dorogokupets/arraytask/creater/impl/ArrayCreatorImpl.java
@@ -0,0 +1,22 @@
+package by.dorogokupets.arraytask.creater.impl;
+
+import by.dorogokupets.arraytask.creater.ArrayCreator;
+import by.dorogokupets.arraytask.entity.DataArray;
+import by.dorogokupets.arraytask.exception.DataException;
+
+import java.util.List;
+
+public class ArrayCreatorImpl implements ArrayCreator {
+    @Override
+    public DataArray createArray(List<Integer> numbers) throws DataException {
+        if (numbers.isEmpty()) {
+            throw new DataException("Empty list.");
+        }
+        int[] array = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            array[i] = number;
+        }
+        return new DataArray(array);
+    }
+}
